86ecdd87062e3c4fb563ef9fd854768eefaaa5da
sadiyabot017 <130077680+sadiyabot017@users.noreply.github.com>
2023-04-09 17:27:08 +0530

    Update config.js

--- config.js
+++ config.js
@@ -21,9 +21,9 @@ global.APIKeys = {
 }
 ////////////////////////////////////////////////////|:|             OWNER DETAILS             |:|//////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-global.session = process.env.SESSION_ID || 'PUT HERE',
-global.ownername = process.env.OWNER_NAME === undefined ? "DarkWinzo" : process.env.OWNER_NAME
-let fake = process.env.OWNER_NUMBER === undefined ? '94775200935' : process.env.OWNER_NUMBER
+global.session = process.env.SESSION_ID || 'QVIwSnE3OTI=',
+global.ownername = process.env.OWNER_NAME === undefined ? "𝐒𝐋 𝐒𝐀𝐃𝐈𝐘𝐀" : process.env.OWNER_NAME
+let fake = process.env.OWNER_NUMBER === undefined ? '94704221020' : process.env.OWNER_NUMBER
 global.owner = [`${fake}`]
 global.ownernumber = [`${fake}`]
 global.premium = [`${fake}`]
